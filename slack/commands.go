a924fc3b0892606f7c8d0ad2094dc22598ee577f
Alex Studer <alex@studer.dev>
2022-09-11 15:50:34 -0400

    slack: allow registering slash command handlers

--- slack/commands.go
+++ slack/commands.go
@@ -1,8 +1,19 @@
 package slack
 
-import "net/url"
+import (
+	"net/url"
+)
 
-type CommandHandler func(text string) (map[string]string, error)
+type CommandContext struct {
+	ResponseURL string
+	ChannelID   ChannelID
+	TeamID      string
+	TriggerID   string
+	UserID      string
+	UserName    string
+}
+
+type CommandHandler func(command string, text string, c CommandContext) (map[string]string, error)
 
 func (c *Client) HandleSlashCommand(data url.Values) (map[string]string, error) {
 	c.commandLock.Lock()
@@ -10,6 +21,12 @@ func (c *Client) HandleSlashCommand(data url.Values) (map[string]string, error) 
 
 	command := data["command"][0]
 	text := data["text"][0]
+	responseURL := data["response_url"][0]
+	channelID := ChannelID(data["channel_id"][0])
+	teamID := data["team_id"][0]
+	triggerID := data["trigger_id"][0]
+	userID := data["user_id"][0]
+	userName := data["user_name"][0]
 
 	handler, ok := c.commandMap[command]
 	if !ok {
@@ -19,10 +36,24 @@ func (c *Client) HandleSlashCommand(data url.Values) (map[string]string, error) 
 		}, nil
 	}
 
-	result, err := handler(text)
+	result, err := handler(command, text, CommandContext{
+		ResponseURL: responseURL,
+		ChannelID:   channelID,
+		TeamID:      teamID,
+		TriggerID:   triggerID,
+		UserID:      userID,
+		UserName:    userName,
+	})
 	if err != nil {
 		return nil, err
 	}
 
 	return result, nil
+}
+
+func (c *Client) RegisterCommandHandler(command string, handler CommandHandler) {
+	c.commandLock.Lock()
+	defer c.commandLock.Unlock()
+
+	c.commandMap[command] = handler
 }
