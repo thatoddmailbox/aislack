d84ade6d34cf5b3b7ec14e0db53b9b69243167fa
Alex Studer <alex@studer.dev>
2022-09-11 16:13:09 -0400

    slack/messages: reaction support

--- slack/messages.go
+++ slack/messages.go
@@ -2,10 +2,12 @@ package slack
 
 import "net/http"
 
+type MessageTS string
+
 type SentMessage struct {
-	Text string `json:"text"`
-	User string `json:"user"`
-	TS   string `json:"ts"`
+	Text string    `json:"text"`
+	User string    `json:"user"`
+	TS   MessageTS `json:"ts"`
 }
 
 type historyResponse struct {
@@ -23,6 +25,32 @@ func (c *Client) ListConversationHistory(channelID ChannelID) ([]SentMessage, er
 	}
 
 	return response.Messages, nil
+}
+
+func (c *Client) ReactToMessage(emoji string, messageTS MessageTS, channelID ChannelID) error {
+	err := c.request(http.MethodPost, "reactions.add", map[string]string{
+		"channel":   string(channelID),
+		"name":      emoji,
+		"timestamp": string(messageTS),
+	}, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) UnreactToMessage(emoji string, messageTS MessageTS, channelID ChannelID) error {
+	err := c.request(http.MethodPost, "reactions.remove", map[string]string{
+		"channel":   string(channelID),
+		"name":      emoji,
+		"timestamp": string(messageTS),
+	}, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (c *Client) PostMessage(channelID ChannelID, text string) error {
